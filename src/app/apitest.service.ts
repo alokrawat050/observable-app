c6d697865eef8d9c1fdeaa98498ccfdc106ff94a
Alok Rawat <alokrawat050@gmail.com>
2018-05-19 20:37:37 +0900

    initial setup and develop simle app with observables

---
+++ src/app/apitest.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Student } from './models/student';
+import { OrderBook } from './models/orderbook';
+import { Rates } from './models/rates';
+import { LatestPrices } from './models/latestprices';
+
+@Injectable()
+export class ApitestService {
+
+  languages:string = 'http://localhost:4201/languages';
+  student:string = 'http://localhost:4201/student';
+  orderBook:string = 'http://localhost:4201/orderbook';
+  rates:string = 'http://localhost:4201/rates';
+  latestPrices:string = 'http://localhost:4201/latestprices';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getLanguages(): Observable<any> {
+    return this.httpClient.get(this.languages);
+  }
+
+  getStudent(): Observable<Student> {
+    return this.httpClient.get<Student>(this.student);
+  }
+
+  getOrderBook(): Observable<OrderBook> {
+    return this.httpClient.get<OrderBook>(this.orderBook);
+  }
+
+  getRates(): Observable<Rates> {
+    return this.httpClient.get<Rates>(this.rates);
+  }
+
+  getLatestPrices(): Observable<LatestPrices> {
+    return this.httpClient.get<LatestPrices>(this.latestPrices);
+  }
+}
